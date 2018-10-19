85113c5874dced484d04d3dea0d42d5b23adeeb5
Unknown <fauconnier85@gmail.com>
2018-10-19 11:26:00 +0200

    debut

---
+++ projetTestUnitaire/projetTestUnitaire/Catapulte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetTestUnitaire
+{
+    public class Catapulte 
+    {
+        int LifePoint;
+
+        public void recupLifePoint()
+        {
+            Spoon spoon = new Spoon();
+        }
+
+    }
+}
